dc1174b4ec516cbaa814e4721c0456d240422ab0
hariharaniyer12 <91085597+hariharaniyer12@users.noreply.github.com>
2021-12-10 22:58:18 +0530

    Update finalpage.java

--- finalpage.java
+++ finalpage.java
@@ -1,1 +1,30 @@
+package com.example.javaproject;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity4 extends AppCompatActivity {
+    Button button;
+    TextView money;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        button=findViewById(R.id.button7);
+        money=findViewById(R.id.textView10);
+        String moneygot=getIntent().getStringExtra("finalmoney");
+        money.setText(moneygot);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
